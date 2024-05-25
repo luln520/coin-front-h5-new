e0a6c7009b71e65c5b20a75eb40cbc4051437910
BaiSeBase <19102664454@163.com>
2024-05-25 12:41:28 +0800

    充值

--- src/pages/rechargeorderlist/components/centerPage/index.tsx
+++ src/pages/rechargeorderlist/components/centerPage/index.tsx
@@ -8,58 +8,50 @@ import { Button } from "antd";
 import { Toast } from "antd-mobile";
 import { useState } from "react";
 
-export default function CenterPage() {
+export default function CenterPage({ czList }) {
   const navigate = useNavigate();
   const { t: translate } = useTranslation();
-
-  return (
-    <div class="rechargeorderlist-1">
-      <div class="rechargeorderlist-2">
-        <ul class="rechargeorderlist-3">
+  //充值列表
+  const getCzNodes = () => {
+    const nodes = [];
+    for (const cz of czList) {
+      const node = (
+        <>
           <li class="rechargeorderlist-4">
             <div class="rechargeorderlist-5">
-              <h1 class="rechargeorderlist-6">100000 USDT</h1>
+              <h1 class="rechargeorderlist-6">
+                {cz.num} {cz.coin.toUpperCase()}
+              </h1>
               <div class="rechargeorderlist-7">
                 <span class="rechargeorderlist-8">
-                  <small class="rechargeorderlist-9">2024-02-13 10:23:19</small>
+                  <small class="rechargeorderlist-9"> {cz.addtime}</small>
                 </span>
               </div>
             </div>
             <div class="rechargeorderlist-10">
-              <p class="rechargeorderlist-11">通過</p>
+              <p class="rechargeorderlist-11">
+                {translate(
+                  getText(
+                    cz.status === 1
+                      ? "待審查"
+                      : cz.status === 2
+                      ? "已完成"
+                      : "失敗"
+                  )
+                )}
+              </p>
             </div>
           </li>
-          <li class="rechargeorderlist-12">
-            <div class="rechargeorderlist-13">
-              <h1 class="rechargeorderlist-14">3000000 USDT</h1>
-              <div class="rechargeorderlist-15">
-                <span class="rechargeorderlist-16">
-                  <small class="rechargeorderlist-17">
-                    2024-02-13 10:19:06
-                  </small>
-                </span>
-              </div>
-            </div>
-            <div class="rechargeorderlist-18">
-              <p class="rechargeorderlist-19">通過</p>
-            </div>
-          </li>
-          <li class="rechargeorderlist-20">
-            <div class="rechargeorderlist-21">
-              <h1 class="rechargeorderlist-22">200000 USDT</h1>
-              <div class="rechargeorderlist-23">
-                <span class="rechargeorderlist-24">
-                  <small class="rechargeorderlist-25">
-                    2024-02-13 10:18:40
-                  </small>
-                </span>
-              </div>
-            </div>
-            <div class="rechargeorderlist-26">
-              <p class="rechargeorderlist-27">通過</p>
-            </div>
-          </li>
-        </ul>
+        </>
+      );
+      nodes.push(node);
+    }
+    return nodes;
+  };
+  return (
+    <div class="rechargeorderlist-1">
+      <div class="rechargeorderlist-2">
+        <ul class="rechargeorderlist-3"> {getCzNodes()}</ul>
       </div>
     </div>
   );

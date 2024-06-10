ed0fffaf137c3113946292355569695024522333
BaiSeBase <19102664454@163.com>
2024-06-10 17:27:05 +0800

    修改

--- src/pages/homecenter/components/optionbox3/index.tsx
+++ src/pages/homecenter/components/optionbox3/index.tsx
@@ -15,7 +15,7 @@ export default function Optionbox3({ loginmsg }) {
       <div className="optionbox3-2">
         <div className="optionbox3-3"></div>
         <img
-          src="/level/20220324030143175182.png"
+          src={la=="zh"?"/level/20220324030143175182.png":"/level/2022032403014737859.png"}
           
           className="optionbox3-4"
           onClick={() => {

fbf38af7cbe6db6aa7d682c49b97b4fcdcda1c46
BaiSeBase <19102664454@163.com>
2024-07-19 00:02:07 +0800

    修改

--- src/components/homeTopBar/index.tsx
+++ src/components/homeTopBar/index.tsx
@@ -7,20 +7,10 @@ export default function HomeTopBar({ companyData }) {
   return (
     <div className="hometopbar-1">
       <div className="hometopbar-2">
-        <div className="hometopbar-3">
           <img
             src={imageConfig.baseImageUrl + companyData?.companyLogoName}
-            className="hometopbar-5"
+            className="hometopbar-3"
           />
-          <div className="hometopbar-6">
-            <div className="hometopbar-7">
-              <div className="hometopbar-8"></div>
-            </div>
-            <div className="hometopbar-9">
-              <div className="hometopbar-10"></div>
-            </div>
-          </div>
-        </div>
       </div>
       <div className="hometopbar-11"></div>
     </div>
